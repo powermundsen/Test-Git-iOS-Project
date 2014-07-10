48fd6a310bef407f3fcf6969410f2e1dbbd8a214
Andy <test@MacBook-Pro.local>
2014-07-10 09:57:51 +0200

    added a test class

---
+++ Github test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Github test
+//
+//  Created by Andy on 10.07.14.
+//  Copyright (c) 2014 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTest : UIView
+
+@end
