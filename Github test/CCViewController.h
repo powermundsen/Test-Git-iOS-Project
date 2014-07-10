f041b0094487edfdb898eb27d8963b04b796efeb
Andy <test@MacBook-Pro.local>
2014-07-10 09:27:45 +0200

    Initial Commit

---
+++ Github test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Github test
+//
+//  Created by Andy on 10.07.14.
+//  Copyright (c) 2014 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Github test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Github test
+//
+//  Created by Andy on 10.07.14.
+//  Copyright (c) 2014 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
